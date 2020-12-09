12e7c3ad3aa6f2657d7c59ff75793346c9c65300
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-09 13:35:20 -0500

    rewrote add function (first one, adding to end)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,4 +6,9 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
+  public boolean add(T value){
+    if (value==null) throw new IllegalArgumentException();
+    super.add(value);
+    return true;
+  }
 }
