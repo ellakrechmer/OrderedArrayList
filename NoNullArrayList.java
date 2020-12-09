aa80a9e0016deffe917e399c4c831c7b9d909220
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-09 13:43:30 -0500

    rewrote set function

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -8,11 +8,14 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
   public boolean add(T value){
     if (value==null) throw new IllegalArgumentException();
-    super.add(value);
-    return true;
+    return super.add(value);
   }
   public void add (int index, T value){
     if (value==null) throw new IllegalArgumentException();
     super.add(index, value);
+  }
+  public T set(int index, T value){
+    if (value==null) throw new IllegalArgumentException();
+    return super.set(index, value);
   }
 }
