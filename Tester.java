02331bbe218097c19d1c4b2690985d736f40c5ec
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-11 12:39:42 -0500

    tested all methods for NoNullArrayList

--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class Tester{
   public static void main(String[] args){
-    
+    ArrayList<String> s= new NoNullArrayList<String>();
+    s.add("uni"); s.add("kani"); s.add("ebi"); //s.set(1, null);
+    System.out.println(s);
+    ArrayList<String> t= new ArrayList<String>();
+    t.add("uni"); t.add("kani"); t.add("ebi"); t.set(1, null);
+    System.out.println(t);
   }
 }
