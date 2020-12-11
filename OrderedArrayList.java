988774b9eb01f2f3b3ea148a8210d531a3a166ca
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-11 17:40:38 -0500

    created and tested add method without index

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,8 +1,32 @@
+import java.util.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   public OrderedArrayList(){
     super();
   }
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
+  }
+  // private void sort(){
+  //   //sorting
+  //   for (int i=0; i<size(); i++){
+  //     for (int j=0; j<size(); j++){
+  //       T value=remove(i);
+  //       if (get(i).compareTo(get(j))>0) super.add(j,value);
+  //     }
+  //   }
+  // }
+  public boolean add (T value){
+    //sort();
+    //adding
+    if (size()==0) {
+      super.add(value);
+      return true;
+    }
+    int i=size()-1;
+    while(i>=1 && value.compareTo(get(i))<=0) {
+        i--;
+    }
+    super.add(i, value);
+    return true;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,11 +1,8 @@
 import java.util.*;
 public class Tester{
   public static void main(String[] args){
-    ArrayList<String> s= new NoNullArrayList<String>();
-    s.add("uni"); s.add("kani"); s.add("ebi"); //s.set(1, null);
+    ArrayList<String> s= new OrderedArrayList<String>();
+    s.add("uni"); s.add("kani"); s.add("ebi"); s.add("ani");
     System.out.println(s);
-    ArrayList<String> t= new ArrayList<String>();
-    t.add("uni"); t.add("kani"); t.add("ebi"); t.set(1, null);
-    System.out.println(t);
   }
 }
