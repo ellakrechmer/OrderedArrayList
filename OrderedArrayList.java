f57db35ce4f38c0969e11f02d97fd5d2fe55e237
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-11 17:47:21 -0500

    found issue with add because sorting isn't quite right

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,18 +6,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
   }
-  // private void sort(){
-  //   //sorting
-  //   for (int i=0; i<size(); i++){
-  //     for (int j=0; j<size(); j++){
-  //       T value=remove(i);
-  //       if (get(i).compareTo(get(j))>0) super.add(j,value);
-  //     }
-  //   }
-  // }
-  public boolean add (T value){
-    //sort();
-    //adding
+  public boolean add(T value){
     if (size()==0) {
       super.add(value);
       return true;
@@ -28,5 +17,16 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     }
     super.add(i, value);
     return true;
+  }
+
+  public void add (int index, T value){
+    if (size()==0) {
+      super.add(value);
+    }
+    int i=size()-1;
+    while(i>=1 && value.compareTo(get(i))<=0) {
+        i--;
+    }
+    super.add(i, value);
   }
 }
--- Tester.java
+++ Tester.java
@@ -3,6 +3,8 @@ public class Tester{
   public static void main(String[] args){
     ArrayList<String> s= new OrderedArrayList<String>();
     s.add("uni"); s.add("kani"); s.add("ebi"); s.add("ani");
+    s.add("apple"); s.add("cat");
+    s.add("banana");
     System.out.println(s);
   }
 }
