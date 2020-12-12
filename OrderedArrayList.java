b5ed30237a084567f364bd71a11e2996ad7e44f2
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-12 10:32:02 -0500

    created a private sort method and got it to work, so now my first add method works

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,27 +6,34 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
   }
+  private void sort(){
+    for (int i=0; i<size(); i++){
+      int minindex=i;
+      for (int j=i+1; j<size(); j++){
+        if (get(j).compareTo(get(minindex))<0) {
+          minindex=j;
+        }
+      }
+      T curr=get(i);
+      set(i, get(minindex));
+      set(minindex, curr);
+    }
+  }
+
   public boolean add(T value){
-    if (size()==0) {
-      super.add(value);
-      return true;
-    }
-    int i=size()-1;
-    while(i>=1 && value.compareTo(get(i))<=0) {
-        i--;
-    }
-    super.add(i, value);
+    super.add(value);
+    sort();
     return true;
   }
 
-  public void add (int index, T value){
-    if (size()==0) {
-      super.add(value);
-    }
-    int i=size()-1;
-    while(i>=1 && value.compareTo(get(i))<=0) {
-        i--;
-    }
-    super.add(i, value);
-  }
+  // public void add (int index, T value){
+  //   if (size()==0) {
+  //     super.add(value);
+  //   }
+  //   int i=size()-1;
+  //   while(i>=1 && value.compareTo(get(i))<=0) {
+  //       i--;
+  //   }
+  //   super.add(i, value);
+  // }
 }
--- Tester.java
+++ Tester.java
@@ -6,5 +6,8 @@ public class Tester{
     s.add("apple"); s.add("cat");
     s.add("banana");
     System.out.println(s);
+    // ArrayList<Integer> n=new OrderedArrayList<Integer>();
+    // n.add(5); n.add(7); n.add(3); n.add(6); n.add(4); n.add(1); n.add(2);
+    // System.out.println(n);
   }
 }
